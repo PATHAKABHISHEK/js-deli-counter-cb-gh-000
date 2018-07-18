089bcd09249799a390024f4bc549556e7b7bfe4d
Abhishek Pathak <pathakabhishek0003@gmail.com>
2018-07-18 10:40:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,13 +4,3 @@ var takeANumber = (line,person) =>{
   line.push(person);
   return "Welcome, "+person+"."+(line.index(person)+1)" You are number "+" in line."
 }
-
-var nowServing = (line) =>{
-  if(line[0] != undefined)
-  {
-    return "Currently serving " + line[0]+"."
-}
-else{
-  return "There is nobody waiting to be served!"
-}
-}
